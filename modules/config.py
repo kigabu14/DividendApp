31be3357274b86b3ff772db1d10fe10a01cbf969
kigabu14 <i.m-sorry@hotmail.com>
2025-07-29 23:39:49 +0700

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,15 @@
+SET100 =[ "PTT.BK", "KBANK.BK", "SCB.BK", "AOT.BK", "ADVANC.BK", "CPALL.BK", "BBL.BK", "GULF.BK",
+    "SCC.BK", "TOP.BK", "PTTEP.BK", "CPN.BK", "BDMS.BK", "KTB.BK", "RATCH.BK", "EGCO.BK",
+    "IVL.BK", "TU.BK", "TTB.BK", "TISCO.BK", "BEM.BK", "AWC.BK", "TRUE.BK", "LH.BK",
+    "BJC.BK", "MINT.BK", "GLOBAL.BK", "HMPRO.BK", "CRC.BK", "BH.BK", "EA.BK", "SAWAD.BK",
+    "KCE.BK", "PLANB.BK", "OSP.BK", "WHA.BK", "JMART.BK", "DELTA.BK", "CBG.BK", "GPSC.BK",
+
+    # SET 51-100 (เฉพาะที่ทำงาน)
+    "BTS.BK", "CENTEL.BK", "BANPU.BK", "BGRIM.BK", "IRPC.BK", "DOHOME.BK",
+    "ORI.BK", "QH.BK", "AMATA.BK", "BA.BK", "CK.BK", "ERW.BK", "PSL.BK",
+    "SPRC.BK", "TCAP.BK", "WHAUP.BK", "CHG.BK", "SIRI.BK", "BCPG.BK",
+    "RS.BK", "TKN.BK", "BEAUTY.BK", "DUSIT.BK", "CPF.BK", "VGI.BK", "SINGER.BK",
+    "MAJOR.BK", "THANI.BK", "BEC.BK", "TPIPL.BK", "FN.BK", "ITD.BK", "MONO.BK",
+    "COM7.BK", "GUNKUL.BK", "NOBLE.BK", "KLINIQ.BK", "SUPER.BK", "STA.BK", "PRIN.BK",
+    "MTC.BK", "FORTH.BK", "TEAM.BK", "COCOCO.BK", "TSE.BK", "AAV.BK",
+    "UV.BK", "TCC.BK", "MGT.BK", "THREL.BK", "BE8.BK", "ANAN.BK", "BCP.BK" ]
